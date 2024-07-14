7aad9b28a55d67af5bcc5cd853f748311345a7ad
masa831 <masa29831@gmail.com>
2024-07-14 12:42:19 +0900

    loguruの情報を追加

---
+++ logging/logger.md
@@ -0,0 +1,12 @@
+# logger
+
+loggerのラッパーライブラリ
+
+<https://qiita.com/ko-he-8/items/3247e16881cf9ba45c8a>
+
+## loguru
+
+loggerのラッパーライブラリの一つ。  
+print代わりにlogを出力させることができる。  
+
+<https://qiita.com/k8uwall/items/07ace17382b454b996a7>
---
+++ logging/test_loguru.py
@@ -0,0 +1,16 @@
+from loguru import logger
+
+
+def main():
+    # logger.add("logtest.log")  # ファイル出力
+    logger.trace("トレース")
+    logger.debug("デバッグ")
+    logger.info("情報")
+    logger.success("成功")
+    logger.warning("警告")
+    logger.error("エラー")
+    logger.critical("クリティカル")
+
+
+if __name__ == '__main__':
+    main()
